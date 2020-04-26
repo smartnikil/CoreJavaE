18b96c3f9e34e683f72d060e4eecbdb1b455842c
Dayakar Prashanth <Dayakar Prashanth@DESKTOP-RL80CPC>
2020-04-27 04:19:03 +0530

    projects101

---
+++ basis of java/src/com/example/Names.java
@@ -0,0 +1,20 @@
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+//input from user
+package com.example;
+
+import java.util.Scanner;
+
+public class Names {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+		System.out.println("first name:");
+		String fn = sc.next();
+		System.out.println("last name:");
+		String ln = sc.next();
+		System.out.println();
+		System.out.println("Hello\n"+fn+" "+ln);
+		
+	};
+
+}
