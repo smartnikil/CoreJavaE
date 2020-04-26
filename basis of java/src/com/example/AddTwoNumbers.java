b06adb032dbb0232ca7fb3528389c234d32859e2
Dayakar Prashanth <Dayakar Prashanth@DESKTOP-RL80CPC>
2020-04-27 04:19:03 +0530

    projects101

---
+++ basis of java/src/com/example/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+// addition of two numbers
+package com.example;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) 
+	{
+	  int firstNumber = 20;
+	  int secondNumber = 20;
+	  System.out.println("sum of two numbers = "+(firstNumber+secondNumber));
+	}
+
+}
---
+++ basis of java/src/com/example/Names.java
@@ -0,0 +1,10 @@
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+package com.example;
+
+public class Names {
+
+	public static void main(String[] args) {
+	System.out.println("Hello\nAlexandra Abramov");
+	};
+
+}
