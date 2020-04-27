42e1e5ccd3fbf1eb7a8a48e1344fbf1bdc9716d2
Dayakar Prashanth <Dayakar Prashanth@DESKTOP-RL80CPC>
2020-04-27 20:48:32 +0530

    addoftwo

---
+++ basis of java/src/com/example/Addoftwo.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Addoftwo {
+		public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("enter your first number:");
+	    int fn = sc.nextInt();
+	    System.out.println("enter your second number:");
+	    int sn = sc.nextInt();
+	    int sum = fn + sn;
+	    System.out.println("sum of two numbers are:"+sum);
+		}
+
+	}
