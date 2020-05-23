a1e4ad388f1d56ce82743dd657dd66221e5b841c
Dayakar Prashanth <Dayakar Prashanth@DESKTOP-RL80CPC>
2020-05-24 01:50:04 +0530

    project

---
+++ basis of java/src/com/example/NormalLoop.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class NormalLoop {
+
+	public static void main(String[] args){
+	int[] arr = {10,20,30,40,50};
+	for (int i=0;i<arr.length;++i)
+	{
+		System.out.println(arr[i]);
+	}	
+	}
+}
