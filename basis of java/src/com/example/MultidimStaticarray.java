8d0fe064e58f4400f2a27c6dd5a7a75863f12204
Dayakar Prashanth <Dayakar Prashanth@DESKTOP-RL80CPC>
2020-05-24 03:30:55 +0530

    project

---
+++ basis of java/src/com/example/MultidimStaticarray.java
@@ -0,0 +1,7 @@
+package com.example;
+public class MultidimStaticarray {
+	public static void main(String[] args) {
+		int[][] arr ={{1,7,6},{3,8,9},{2,4,5},{12,54,65}};
+		System.out.println(arr[3][2]);
+	}
+}
