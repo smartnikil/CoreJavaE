13a5ff4c1da978ceaac9d0505cc2cb6bed1eefc4
Dayakar Prashanth <Dayakar Prashanth@DESKTOP-RL80CPC>
2020-05-24 02:27:27 +0530

    project

---
+++ basis of java/src/com/example/Continuemethod.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Continuemethod {
+
+	public static void main(String[] args) {
+		for (int i=0;i<=10;i++)
+		{
+		if(i==3)
+		//break;
+		continue;	
+		System.out.println(i);
+		}
+	}
+}
