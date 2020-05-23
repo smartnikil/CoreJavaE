2c27ad84e2fb211ece6bf6e296638e8a4d1b2e18
Dayakar Prashanth <Dayakar Prashanth@DESKTOP-RL80CPC>
2020-05-24 02:11:00 +0530

    project

---
+++ basis of java/src/com/example/Foreachloop.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Foreachloop {
+	public static void main(String[] args) {
+		int[] arr = {230000,74646,375537523,489374847};
+		for(int i:arr)
+		{
+			System.out.println(i);
+		}
+	}
+}
